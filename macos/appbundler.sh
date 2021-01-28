4fc5445e56593f17687b45f1a38131fd37929e21
Munehiro Yamamoto <munepixyz@gmail.com>
2021-01-28 15:26:34 +0900

    appbundler.sh: set PATH from TLPath via AppleScript

--- macos/appbundler.sh
+++ macos/appbundler.sh
@@ -8,23 +8,19 @@ BUILDROOT=$(dirname $0)
 # アプリケーションの作成
 rm -rf ${BUILDROOT}/${app_name}
 osacompile -o ${BUILDROOT}/${app_name} <<__APPLESCRIPT__
-on main(input)
+on main()
     try
         tell application "Terminal"
-            -- do script "echo" & space & input & space & "2>&1"
-            -- do script "pwd" -- ==> /Users/foo
-            -- do script "echo \`dirname" & space & input & "\`"
-            do script ". /Applications/${app_name}/Contents/Resources/runketcindy.sh; cd \`dirname" & space & input & "NO_KETCINDY__NO_LIFE\`"
+            set appPath to (path to current application as text)
+            set appPath to quoted form of (POSIX path of appPath)
+            set TLPath to appPath & "texlive/bin/x86_64-darwin"
+            do script "export PATH=" & TLPath & ":/usr/bin:/bin:/usr/sbin:/sbin;  . " & appPath & "/Contents/Resources/runketcindy.sh; cd \${HOMEDIR}"
         end tell
     end try
 end main
 
-on open argv
-    main(quoted form of POSIX path of (item 1 of argv))
-end open
-
 on run
-    main(quoted form of POSIX path of (choose folder))
+    main()
 end run
 __APPLESCRIPT__
 
