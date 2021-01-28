ffa2a707c62bb1bc99806a55e13396c69e6bfafb
Munehiro Yamamoto <munepixyz@gmail.com>
2021-01-28 14:38:12 +0900

    appbundler.sh: install internal starter script

--- macos/appbundler.sh
+++ macos/appbundler.sh
@@ -14,7 +14,7 @@ on main(input)
             -- do script "echo" & space & input & space & "2>&1"
             -- do script "pwd" -- ==> /Users/foo
             -- do script "echo \`dirname" & space & input & "\`"
-            do script ". /Applications/${app_name}/setup.sh; cd \`dirname" & space & input & "NO_KETCINDY__NO_LIFE\`"
+            do script ". /Applications/${app_name}/Contents/Resources/runketcindy.sh; cd \`dirname" & space & input & "NO_KETCINDY__NO_LIFE\`"
         end tell
     end try
 end main
@@ -28,11 +28,13 @@ on run
 end run
 __APPLESCRIPT__
 
-## アイコン置き換え
-if [ -f ${BUILDROOT}/../artwork/ketcindy.icns ]; then
-    cp -fva ${BUILDROOT}/../artwork/ketcindy.icns \
-       ${BUILDROOT}/${app_name}/Contents/Resources/droplet.icns
-fi
+## replace default icon with our project icon
+cp -fva ${BUILDROOT}/../artwork/ketcindy.icns \
+   ${BUILDROOT}/${app_name}/Contents/Resources/droplet.icns
+
+## install internal starter script
+cp -fva ${BUILDROOT}/runketcindy.sh \
+   ${BUILDROOT}/${app_name}/Contents/Resources/
 
 echo Finished
 exit
