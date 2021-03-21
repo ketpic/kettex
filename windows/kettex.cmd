6636d8e63c0e9b9fff1d260ece890129660dcf56
Munehiro Yamamoto <munepixyz@gmail.com>
2021-03-22 00:26:21 +0900

    kettex.cmd: keep cmd.exe just after running KeTTeX startar script kettexinst.cmd.in: get current directory of the installer

--- windows/kettex.cmd
+++ windows/kettex.cmd
@@ -3,7 +3,9 @@
 set PATH=C:\kettex\texlive\bin\win32;%SystemRoot%\system32;%SystemRoot%;
 
 echo KeTTeX terminal
-echo
+echo.
 echo No KeTCindy, No Life
 echo Happy KeTTeXing!
-echo
+echo.
+
+cmd /k
